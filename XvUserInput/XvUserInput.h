6475704f70c7e4242661fead4e8b7ef2f0d3b06c
jasonjsnell <snell.jason@gmail>
2017-07-09 09:01:09 +0200

    Initial Commit

---
+++ XvUserInput/XvUserInput.h
@@ -0,0 +1,19 @@
+//
+//  XvUserInput.h
+//  XvUserInput
+//
+//  Created by Jason Snell on 7/9/17.
+//  Copyright © 2017 Jason J. Snell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for XvUserInput.
+FOUNDATION_EXPORT double XvUserInputVersionNumber;
+
+//! Project version string for XvUserInput.
+FOUNDATION_EXPORT const unsigned char XvUserInputVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <XvUserInput/PublicHeader.h>
+
+
